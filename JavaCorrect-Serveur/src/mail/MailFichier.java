eca8883f60b07ceb0a84e555cc3bef68122db316
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2018-01-24 15:35:21 +0100

    Correction d'un bug des alertes dans l'écran register

--- JavaCorrect-Serveur/src/mail/MailFichier.java
+++ JavaCorrect-Serveur/src/mail/MailFichier.java
@@ -1,21 +1,21 @@
 package mail;
 
 public class MailFichier {
-
-	public static void main(String[] args) {
-
+	
+	public static void main(final String[] args) {
+		
 		// while (true) {
 		try {
 			ReceiveEmail.receiveEmail("java.correct@gmail.com", "789@Upmc");
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		// }
-
+		
 		// SendEmail.sendEmail("java.correct@gmail.com", "789@Upmc",
 		// "amichi.katia@gmail.com", "titre", "message test");
-
+		
 	}
-
+	
 }
