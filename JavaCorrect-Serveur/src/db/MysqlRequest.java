c9ef7d6241ac8a9213ecdf66d4dcef665395f80f
katia <27005881+KatiaAMICHI@users.noreply.github.com>
2018-01-25 17:15:29 +0100

    ajout de la bd

--- JavaCorrect-Serveur/src/db/MysqlRequest.java
+++ JavaCorrect-Serveur/src/db/MysqlRequest.java
@@ -1,7 +1,37 @@
 package db;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class MysqlRequest {
 	private static final Connection myqlco = MysqlConnexion.getInstance(MysqlPropertiesParser.getInstance());
+
+	public static ResultSet getEmailByidProjet(String idProjet) throws CSVSavingException {
+
+		try {
+			String getMailByidProjet = "SELECT emailEtu from ETUDIANT WHERE numEtu IN (SELECT numEtu from EVALUATION where EVALUATION.PROJET_idProjet= ?);";
+			PreparedStatement preparedstatement = myqlco.prepareStatement(getMailByidProjet);
+			preparedstatement.setString(1, idProjet);
+			return preparedstatement.executeQuery();
+		} catch (SQLException e) {
+			throw new CSVSavingException(e);
+		}
+	}
+
+	public static ResultSet getDateExpiByidProjet(String idProjet) throws SQLException {
+		String getProjectRs = "select dateExpi " + "FROM PROJET " + "where idProjet = ?;";
+		PreparedStatement preparedstatement = myqlco.prepareStatement(getProjectRs);
+		preparedstatement.setString(1, idProjet);
+		return preparedstatement.executeQuery();
+	}
+
+	public static ResultSet getidEtuByEmail(String email) throws SQLException {
+		String getProjectRs = "select idEtu " + "FROM ETUDIANT " + "where emailEtu = ?;";
+		PreparedStatement preparedstatement = myqlco.prepareStatement(getProjectRs);
+		preparedstatement.setString(1, email);
+		return preparedstatement.executeQuery();
+	}
+
 }
