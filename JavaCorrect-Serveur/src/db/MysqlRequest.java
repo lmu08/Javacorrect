54b0db6dab8cf45ca3351481cc2964aac20bb2e5
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2018-01-25 16:15:41 +0100

    Ajout de la requête pour vérifier l'ID d'un projet

--- JavaCorrect-Serveur/src/db/MysqlRequest.java
+++ JavaCorrect-Serveur/src/db/MysqlRequest.java
@@ -1,7 +1,16 @@
 package db;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MysqlRequest {
 	private static final Connection myqlco = MysqlConnexion.getInstance(MysqlPropertiesParser.getInstance());
+	
+	public static boolean checkProjectId(final String id) throws SQLException {
+		final String request = "SELECT * FROM javacorrectdb.PROJET where idProjet=" + id;
+		final Statement statement = myqlco.createStatement();
+		statement.execute(request);
+		return statement.getResultSet().first(); //returns true if a match is found
+	}
 }
