fc8b162aef6d1f3e056e9734067927452fe6f5e1
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2018-01-25 17:39:22 +0100

    Mise à jour de la requete de vérification de l'entête du mail

--- JavaCorrect-Serveur/src/db/MysqlRequest.java
+++ JavaCorrect-Serveur/src/db/MysqlRequest.java
@@ -1,16 +1,23 @@
 package db;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class MysqlRequest {
 	private static final Connection myqlco = MysqlConnexion.getInstance(MysqlPropertiesParser.getInstance());
 	
-	public static boolean checkProjectId(final String id) throws SQLException {
-		final String request = "SELECT * FROM javacorrectdb.PROJET where idProjet=" + id;
-		final Statement statement = myqlco.createStatement();
-		statement.execute(request);
-		return statement.getResultSet().first(); //returns true if a match is found
+	public static boolean checkProjectId(final String projetId, String studentId) throws SQLException {
+		final String request = "SELECT * FROM javacorrectdb.EVALUATION "
+		+ "WHERE PROJET_idProjet=? "
+		+ "AND ETUDIANT_numEtu IN "
+			+ "(SELECT * FROM (SELECT numetu "
+				+ "FROM javacorrectdb.ETUDIANT "
+				+ "WHERE idEtu=?) "
+			+ "AS subquery);";
+		final PreparedStatement statement = myqlco.prepareStatement(request);
+		statement.setString(1, projetId);
+		statement.setString(2, studentId);
+		return statement.executeQuery().first(); //returns true if a match is found
 	}
 }
