e34d3bfbc470e4dcacbf47c69703e53668f40993
katia <27005881+KatiaAMICHI@users.noreply.github.com>
2018-01-25 20:20:26 +0100

    aucun changement

--- JavaCorrect-App/src/controller/Main.java
+++ JavaCorrect-App/src/controller/Main.java
@@ -5,15 +5,16 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main
-extends Application {
+public class Main extends Application {
 
 	public static void main(final String[] args) {
-		//		Thread receiveInputFile = new Thread(new ReceiveInputFileSocket(52112, "/home/flo"));
-		//		receiveInputFile.start();
+		// Thread receiveInputFile = new Thread(new ReceiveInputFileSocket(52112,
+		// "/home/flo"));
+		// receiveInputFile.start();
 		Application.launch(args);
+		System.out.println();
 	}
-	
+
 	@Override
 	public void start(final Stage primaryStage) {
 		primaryStage.setTitle("Javacorrect");
