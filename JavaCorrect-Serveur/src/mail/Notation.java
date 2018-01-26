79cef460cd694de871b708d653cccb30a164a262
Lucas MATHIEU <28020294+lmu08@users.noreply.github.com>
2018-01-26 18:57:22 +0100

    Prise en compte des retards

--- JavaCorrect-Serveur/src/mail/Notation.java
+++ JavaCorrect-Serveur/src/mail/Notation.java
@@ -6,16 +6,18 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.ResultSet;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import db.MysqlRequest;
 
 public class Notation {
-	
+
 	private final static String SEPARATOR = "/";
-	
+
 	private static final Runtime RUNTIME = Runtime.getRuntime();
-	
+
 	/**
 	 * noter les devoir des étudiants
 	 *
@@ -23,12 +25,18 @@ public class Notation {
 	 *        répertoire où le résulat de la compilation sera mit
 	 * @param numEtu
 	 *        numéro de l'étudiant a noté
+	 * @param date
 	 * @throws IOException
 	 */
-	public static void note(final String compilDirectory, final String numEtu, final String idProjet, final String args)
+	public static void note(final String compilDirectory, final String numEtu, final String idProjet, final String args, final Date date)
 	throws Exception {
 		// le répertoi de l'étudiant
 		final String etuDirectory = compilDirectory + SEPARATOR + numEtu;
+		final ResultSet projectDateRs = MysqlRequest.getDateExpiByidProjet(idProjet);
+		projectDateRs.next();
+		final java.sql.Date projectDate = projectDateRs.getDate("dateExpi");
+		final boolean malus = projectDate.before(date);
+
 		RUNTIME.exec("./javacShell " + etuDirectory + " " + args);
 		final String output = Files.list(Paths.get(compilDirectory)).map(Path::getFileName).map(Path::toString).filter(path -> path.endsWith(".txt")).findAny().orElse("");
 		System.out.println("Comparaison avec le fichier du prof : " + output);
@@ -40,10 +48,10 @@ public class Notation {
 			MysqlRequest.updateNote(0.0, idProjet, Integer.valueOf(numEtu));
 		} else {
 			System.out.println("20");
-			MysqlRequest.updateNote(20.0, idProjet, Integer.valueOf(numEtu));
+			MysqlRequest.updateNote(malus ? 15.0 : 20.0, idProjet, Integer.valueOf(numEtu));
 		}
 	}
-	
+
 	// br2 reçois true si les fichier sont déffirents / false dans le cas contraire
 	public static boolean diffFichier(final String cmd)
 	throws Exception {
--- JavaCorrect-Serveur/src/mail/ReceiveEmail.java
+++ JavaCorrect-Serveur/src/mail/ReceiveEmail.java
@@ -2,7 +2,9 @@ package mail;
 
 import java.io.File;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -27,112 +29,115 @@ import db.MysqlRequest;
 
 public class ReceiveEmail {
 	private final static String SEPARATOR = "/";
-
+	
 	static String saveDirectory;
-
+	
 	public static void receiveEmail(final String login, final String password)
 	throws Exception {
-		
+
 		final Properties imapProps = MailPropertiesParser.getInstance().getImapProperties();
 		List<Thread> listThread = new ArrayList<>();
-		
+
 		try {
 			final IMAPStore emailStore = (IMAPStore) Session.getInstance(imapProps).getStore("imap");
 			emailStore.connect(login, password);
-			
+
 			// create the folder object and open it
 			final Folder emailFolder = emailStore.getFolder("INBOX");
 			emailFolder.open(Folder.READ_WRITE);
-			
+
 			// retrieve the messages from the folder in an array and print it
 			final Message[] messages = emailFolder.getMessages();
 			for (int i = 0; i < messages.length; i++) {
-				
+
 				final Message message = messages[i];
-				
+
 				final Thread e = emailcontrol(emailFolder, login, password, message);
 				listThread = new LinkedList<>();
 				listThread.add(e);
 			}
-			
+
 			// attente des threads
 			for (final Thread thread : listThread) {
 				thread.join();
 			}
-			
+
 			if (emailFolder.isOpen()) {
 				emailFolder.close(false);
 			}
 			emailStore.close();
-			
+
 		} catch (final MessagingException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-
+	
 	private static Thread emailcontrol(final Folder emailFolder, final String login, final String password, final Message message) {
-
+		
 		final Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
-
+				
 				if (true) {
 					try {
-
+						
 						if (!emailFolder.isOpen()) {
 							emailFolder.open(Folder.READ_WRITE);
 						}
 
+						final SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+						final Date date = format.parse(format.format(message.getReceivedDate()));
+
 						final String subject = message.getSubject();
 						System.out.println("sub : " + subject);
-
+						
 						final Pattern p = Pattern.compile(SEPARATOR);
 						// séparation du subject en sous-chaînes
 						final String[] items = p.split(subject, 10);
-
+						
 						// f > chemin home (ex içi c'est /home/katy
 						final FileSystemView fsv = FileSystemView.getFileSystemView();
 						final File f = fsv.getDefaultDirectory();
-
+						
 						if (items.length >= 2) {
 							final boolean matchSubj = MysqlRequest.checkProjectId(items[0], items[1]);
-
+							
 							if (matchSubj) {
-
+								
 								final String idProjet = items[0];
 								saveDirectory = f.toString() + SEPARATOR + idProjet; // le répertoire du dossier du projet
 																						// du prof
-
+								
 								final Multipart multipart = (Multipart) message.getContent();
 								System.out.println("nb de pièce joint : " + (multipart.getCount() - 1));
-
+								
 								for (int j = 1; j < multipart.getCount(); j++) {
-
+									
 									final BodyPart bodyPart = multipart.getBodyPart(j);
 									// InputStream stream = bodyPart.getInputStream();
 									// BufferedReader br = new BufferedReader(new InputStreamReader(stream));
 									final MimeBodyPart part = (MimeBodyPart) multipart.getBodyPart(j);
-
+									
 									final String zipFile = Optional.ofNullable(bodyPart.getFileName()).orElse("");
-
+									
 									// compilation de la regex
 									final Pattern patternFile = Pattern.compile("^[0-9]{7}+.zip$");
 									// création d'un moteur de recherche
 									final Matcher matchNomFileZipe = patternFile.matcher(zipFile);
-
+									
 									System.out.println("si c'est le bon .zip : " + matchNomFileZipe.matches());
-
+									
 									if (matchNomFileZipe.matches() /* && le numEtu correspond à L'idEtu */) {
-
+										
 										final String numEtu = zipFile.substring(0, zipFile.indexOf("."));
-
+										
 										// création du dossier de l'étudiant
 										new File(saveDirectory + SEPARATOR + numEtu).mkdir();
-
+										
 										final String messageContent = part.getContent().toString();
 										System.out.println("Message : " + messageContent);
-
+										
 										// move Files
 										if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
 											// this part is attachment
@@ -142,19 +147,19 @@ public class ReceiveEmail {
 											// this part may be the message content
 											System.out.println(messageContent);
 										}
-
+										
 										// Unzip
 										final File zipFilePath = new File(saveDirectory + SEPARATOR + zipFile);
 										final File destDir = new File(saveDirectory + SEPARATOR + numEtu);
-
+										
 										MyZip.decompress(zipFilePath, destDir, true);
 										final ResultSet rsArgs = MysqlRequest.getArguments(idProjet);
 										rsArgs.next();
 										final String args = rsArgs.getString("arguments");
 										System.out.println(saveDirectory);
-
-										Notation.note(saveDirectory, numEtu, idProjet, args);
-
+										
+										Notation.note(saveDirectory, numEtu, idProjet, args, date);
+										
 										SendEmail.sendEmail(login, password, message.getFrom()[0].toString(), subject,
 											"votre devoir à bien été reçu");
 									} else {
